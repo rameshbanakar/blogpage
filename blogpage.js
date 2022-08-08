e58c635b015e548c201ca1ea89cc88146873efd7
ramesh <ramesh.s.b@campusuvce.in>
2022-08-09 01:14:12 +0530

    login page added

--- blogpage.js
+++ blogpage.js
@@ -3,3 +3,19 @@ function notify(){
     alert(message);
 }
 document.getElementById("blogme").onclick=notify;
+
+//log in validation fuction
+function loginvalid(){
+    if(document.myform.username.value==""){
+        alert("please provide User Name")
+        document.myform.username.focus()
+        return false
+    }
+    else if(document.myform.password.value==""){
+        alert("please enter the password")
+        document.myform.password.focus()
+        return false
+
+    }
+    else{return true}
+}
